803511aece435da6ca6493241feb9f7ab6e530b1
Agazigi <zhanggaosheng603@nenu.edu.cn>
2026-01-21 00:46:50 +0800

    1

--- quartz/util/theme.ts
+++ quartz/util/theme.ts
@@ -155,11 +155,6 @@ ${stylesheet.join("\n\n")}
   --highlight: ${theme.colors.lightMode.highlight};
   --textHighlight: ${theme.colors.lightMode.textHighlight};
 
-  // --titleFont: "${getFontSpecificationName(theme.typography.title || theme.typography.header)}", ${DEFAULT_SANS_SERIF};
-  // --headerFont: "${getFontSpecificationName(theme.typography.header)}", ${DEFAULT_SANS_SERIF};
-  // --bodyFont: "${getFontSpecificationName(theme.typography.body)}", ${DEFAULT_SANS_SERIF};
-  // --codeFont: "${getFontSpecificationName(theme.typography.code)}", ${DEFAULT_MONO};
-
   --headerFont: "LXGW WenKai";
 	--bodyFont: "LXGW WenKai";
 	--codeFont: "Maple Mono CN","LXGW WenKai";
